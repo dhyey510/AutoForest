48facbce4dce6e77988636e5e87bdb70ef8884cd
dhyey510 <dhyey870@gmail.com>
2021-02-11 00:17:07 +0530

    first

---
+++ assets/JS/index.js
@@ -0,0 +1,7 @@
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function togglepopup(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
